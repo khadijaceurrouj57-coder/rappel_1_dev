24fecbff2c7095ea07aac65fbcf19f5965b375f1
khadija <khadijagerrouj4@gmail.com>
2026-09-09 13:23:18 +0100

    commmit

--- maximum.js
+++ maximum.js
@@ -1,1 +1,13 @@
 let nombres = [12, 5, 27, 9, 18];
+
+console.log("Tableau :", nombres);
+
+let maximum = nombres[0];
+
+for (let i = 1; i < nombres.length; i++) {
+    if (nombres[i] > maximum) {
+        maximum = nombres[i];
+    }
+}
+
+console.log("La plus grande valeur est :", maximum);
